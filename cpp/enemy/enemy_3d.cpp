4e93098d82b8ea1a676a2652dc7f8554bf3375e1
syareene <soramame3468@icloud.com>
2025-09-01 13:01:53 +0900

    3dとビルボードとのファイルを分けるためにとりあえずコピー

---
+++ cpp/enemy/enemy_3d.cpp
@@ -0,0 +1,61 @@
+﻿#include "main.h"
+#include "renderer.h"
+#include "enemy/enemy.h"
+#include "modelRenderer.h"
+#include "input.h"
+#include "object/camera.h"
+#include "manager.h"
+
+void Enemy::Init()
+{
+	// モデル読み込み
+	m_ModelRenderer = new ModelRenderer();
+	m_ModelRenderer->Load("asset\\model\\player.obj");
+
+	Renderer::CreateVertexShader(&m_VertexShader, &m_VertexLayout, "shader\\unlitTextureVS.cso");
+
+	Renderer::CreatePixelShader(&m_PixelShader, "shader\\unlitTexturePS.cso");
+
+	AddTag("InGame");
+	AddTag("Enemy");
+}
+
+void Enemy::Uninit()
+{
+	delete m_ModelRenderer;
+	m_VertexLayout->Release();
+	m_VertexShader->Release();
+	m_PixelShader->Release();
+}
+
+void Enemy::Update()
+{
+	// vector3が間違ってる可能性とかもある?
+
+	Camera* camera = Manager::GetCurrentScene()->GetGameObject<Camera>();
+	Vector3 rotation = camera->GetRotation();
+
+	// ここonとM_PIでバグるね->いらない?
+	//camera->SetTarget(GetPosition() + Vector3(0.0f, 1.5f, 0.0f));
+}
+
+void Enemy::Draw()
+{
+	// 入力レイアウト設定
+	Renderer::GetDeviceContext()->IASetInputLayout(m_VertexLayout);
+
+	// シェーダー設定
+	Renderer::GetDeviceContext()->VSSetShader(m_VertexShader, NULL, 0);
+	Renderer::GetDeviceContext()->PSSetShader(m_PixelShader, NULL, 0);
+
+	// 移動、回転マトリックス設定
+	XMMATRIX trans, world, rot, scale;
+	trans = XMMatrixTranslation(GetPosition().x, GetPosition().y, GetPosition().z);
+	//rot = XMMatrixRotationRollPitchYaw(GetRotation().x, GetRotation().y, GetRotation().z);
+	rot = XMMatrixRotationRollPitchYaw(GetRotation().x, GetRotation().y + XM_PI, GetRotation().z);
+	scale = XMMatrixScaling(GetScale().x, GetScale().y, GetScale().z);
+	world = scale * rot * trans;
+	Renderer::SetWorldMatrix(world);
+
+	m_ModelRenderer->Draw();
+}
